80ee4b0d04bbdcb52ecac4d4a272e6146375b90f
GabrielaCedillo <gabychubis.cf@gmail.com>
2025-09-08 09:04:13 -0600

    Tarea de ProgramacionWeb 2

---
+++ Comparación3Numeros.js
@@ -0,0 +1,22 @@
+// Generar número secreto entre 1 y 10
+let numeroSecreto = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+console.log("Número secreto:", numeroSecreto); // Solo para pruebas
+
+// Pedir número al usuario
+let numeroUsuario = parseInt(prompt("Adivina el número secreto del 1 al 10"));
+let vidas = 3;
+
+// Mientras el usuario no adivine y tenga vidas
+while (numeroUsuario !== numeroSecreto && vidas > 1) {
+    vidas--;
+    numeroUsuario = parseInt(prompt("Intenta nuevamente. Te quedan " + vidas + " vidas"));
+}
+
+// Verificar si ganó o perdió
+if (numeroUsuario === numeroSecreto) {
+    console.log("¡Ganaste!");
+    alert("¡Ganaste!");
+} else {
+    console.log("Perdiste");
+    alert("Perdiste");
+}
